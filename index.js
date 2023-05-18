72974d1863ba41e450657739fc57e7cc0420bab7
Mahdi Hasan <mahdihasan10599@gmail.com>
2023-05-18 22:25:42 +0600

    Inserted some toys documents to mongoDb and created an api to get that data from mongoDb and serve it to client side

--- index.js
+++ index.js
@@ -36,6 +36,14 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    
+    const toysCollection = client.db('toysDB').collection('toys');
+
+    // GET (for all toys)
+    app.get('/toys', async(req, res) => {
+        const result = await toysCollection.find().toArray();
+        res.send(result);
+    })
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
