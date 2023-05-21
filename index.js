c150f5ed2320f95a8de9e65febfe27720e784523
Mahdi Hasan <mahdihasan10599@gmail.com>
2023-05-21 20:49:17 +0600

    Added some code inside '/toys' routed api to read limited document from MongoDb using limit()

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ app.use(express.json());
 
 // Enable CORS == Solve the proble 'Browser stop the fetch request or unable to fetch
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 });
 
 // MongoDB integration
@@ -36,39 +36,49 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    
+
     //All Toys Collection 
     const toysCollection = client.db('toysDB').collection('toys');
     // Gallery Image Collection
     const gallerImgCollection = client.db('galleryDB').collection('gallery');
 
     // GET => API for reading toy document from MongoDB
-    app.get('/toys', async(req, res)=> {
+    app.get('/toys', async (req, res) => {
       const category = req.query.category;
-      if(category){
-        const query = {category: category};
+      const limit = parseInt(req.query.limit);
+      if (category) {
+        const query = { category: category };
         const result = await toysCollection.find(query).toArray();
-        res.send(result);
-      }else{
+        res.send(result)
+        return;
+      }
+      else if (limit) {
+        const result = await toysCollection.find().limit(limit).toArray();
+        res.send(result)
+        return;
+      }
+      else {
         const result = await toysCollection.find().toArray();
-        res.send(result);
+        res.send(result)
+        return;
       }
     })
 
     // GET => API for reading a single Toy document from MongoDB
-    app.get('/toy/:id', async(req, res) => {
+    app.get('/toy/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await toysCollection.findOne(query);
       res.send(result);
     })
 
     // GET => Read Gallery
-    app.get('/gallery', async(req, res) => {
+    app.get('/gallery', async (req, res) => {
       const result = await gallerImgCollection.find().toArray();
       res.send(result)
     })
-     
+
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -78,9 +88,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Wellcome to Toy Town Express Server')
+  res.send('Wellcome to Toy Town Express Server')
 })
 
-app.listen(port, ()=> {
-    console.log(`The server is running on the port: ${port}`);
+app.listen(port, () => {
+  console.log(`The server is running on the port: ${port}`);
 })
